32a533c3471afc7c00aa75cbe8e4f3cd6cecd623
Denesh Shan <denesh.shan@audioboo.fm>
2015-02-18 22:12:20 +0000

    Completed table formatter

--- book/issues/lib/issues/cli.ex
+++ book/issues/lib/issues/cli.ex
@@ -1,4 +1,5 @@
 defmodule Issues.CLI do
+  import TableFormatter, only: [ print_table_for_columns: 2 ]
 
   @default_count 4
 
@@ -11,8 +12,6 @@ defmodule Issues.CLI do
     argv
      |> parse_args
      |> process
-     |> convert_to_list_of_hashdicts
-     |> sort_into_ascending_order
   end
 
   @doc """
@@ -44,9 +43,13 @@ defmodule Issues.CLI do
   end
 
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response
+    |> convert_to_list_of_hashdicts
+    |> sort_into_ascending_order
+    |> Enum.take(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def decode_response({:ok, body}), do: body
--- book/issues/test/cli_test.exs
+++ book/issues/test/cli_test.exs
@@ -1,7 +1,9 @@
 defmodule CliTest do
   use ExUnit.Case
 
-  import Issues.CLI, only: [parse_args: 1]
+  import Issues.CLI, only: [
+    parse_args: 1, sort_into_ascending_order: 1, convert_to_list_of_hashdicts: 1
+  ]
 
   test ":help returned by option parsing with -h and --help options" do
     assert parse_args(["-h", "anything"]) == :help
@@ -18,7 +20,13 @@ defmodule CliTest do
 
   test "sort ascending orders the correct way" do
     result = sort_into_ascending_order(fake_created_at_list(["c", "b", "a"]))
-    issues = for issue <- issues, do: issues["created_at"]
+    issues = for issue <- result, do: issue["created_at"]
     assert issues == ~w{a b c}
+  end
+  
+  defp fake_created_at_list(values) do
+    data = for value <- values,
+      do: [{"created_at", value}, {"other_data", "xxx"}]
+    convert_to_list_of_hashdicts data
   end
 end
